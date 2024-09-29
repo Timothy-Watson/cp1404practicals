a8b868d5426af357eac04986bfb8777f8ca32859
Timothy Watson <timothywatson07@gmail.com>
2024-09-29 20:47:54 +1000

    Added menu

---
+++ prac_02/score_menu.py
@@ -0,0 +1,15 @@
+"""
+CP1404 practical
+score program with a menu
+
+Menu:
+(G)et a valid score (must be 0-100 inclusive)
+(P)rint result (copy or import your function to determine the result from score.py)
+(S)how stars (this should print as many stars as the score)
+(Q)uit
+"""
+
+MENU = f"(G)et score\n(P)rint result\n(S)how stars\n(Q)uit"
+
+
+print(MENU)
