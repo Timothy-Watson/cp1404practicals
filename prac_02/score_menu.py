555806c6e3fe4527e7fd9162c75e199ceab8e283
Timothy Watson <timothywatson07@gmail.com>
2024-09-29 20:52:57 +1000

    finished the main menu

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -12,4 +12,32 @@ Menu:
 MENU = f"(G)et score\n(P)rint result\n(S)how stars\n(Q)uit"
 
 
-print(MENU)
+def main():
+    print(MENU)
+    choice = input(">>>").upper()
+    while choice != "Q":
+        if choice == "G":
+            pass
+        elif choice == "P":
+            pass
+        elif choice == "S":
+            pass
+        else:
+            print("Invalid Input")
+        print(MENU)
+        choice = input(">>>").upper()
+    print("Farewell!")
+
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
