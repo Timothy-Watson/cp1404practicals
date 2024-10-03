343f6ef3aea9594077023a2754aa11dfd57bb9b8
Timothy Watson <timothywatson07@gmail.com>
2024-10-03 21:35:07 +1000

    Add commas as a thousands separator

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -34,8 +34,7 @@ for i, number in enumerate(numbers, 1):
 
 # TODO: Use f-string formatting to produce the output:
 # 1922 Gibson L-5 CES for about $16,036!
-cost = int(cost)
-print(f"{year} {name} for about ${cost[-1, -4]}")
+print(f"{year} {name} for about ${cost:,}")
 
 # TODO: Using a for loop with the range function and f-string formatting,
 # produce the following right-aligned output (DO NOT use a list):
