9d0a02068e3947da88116e2b03e43f3ba09e0f78
Timothy Watson <timothywatson07@gmail.com>
2024-10-03 23:08:34 +1000

    Add program03

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,27 +2,31 @@
 CP1404/CP5632 - Practical
 Different file
 """
-FILENAME = "name.txt"
+FILENAME01 = "name.txt"
+FILENAME02 = "numbers.txt"
 
 
 def program01():
     """Gets a name and prints it to a file"""
     name = input("Name: ")
-    out_file = open(FILENAME, 'w')
+    out_file = open(FILENAME01, 'w')
     print(name, file=out_file)
     out_file.close()
 
 
 def program02():
     """"""
-    in_file = open(FILENAME, 'r')
+    in_file = open(FILENAME01, 'r')
     print(f"Hi {in_file.readline().strip()}!")
     in_file.close()
 
 
 def program03():
-    pass
+    with open(FILENAME02) as in_file:
+        result = int(in_file.readline()) + int(in_file.readline())
+        print(result)
 
 
-program01()
-program02()
+# program01()
+# program02()
+program03()
