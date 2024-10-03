8c9539ccb4d83593b4c80e8ec0996189a6e47645
Timothy Watson <timothywatson07@gmail.com>
2024-10-03 23:01:33 +1000

    Add second program

---
+++ prac_03/files.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 - Practical
+Different file
+"""
+FILENAME = "name.txt"
+
+
+def program01():
+    """Gets a name and prints it to a file"""
+    name = input("Name: ")
+    out_file = open(FILENAME, 'w')
+    print(name, file=out_file)
+    out_file.close()
+
+
+def program02():
+    """"""
+    in_file = open(FILENAME, 'r')
+    print(f"Hi {in_file.readline().strip()}!")
+    in_file.close()
+
+
+def program03():
+
+
+
+program01()
+program02()
