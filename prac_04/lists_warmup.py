0ca10ef880d5d6b6ba98f37d0f892a8ed38a4c29
Timothy Watson <timothywatson07@gmail.com>
2024-10-13 22:06:12 +1000

    Add my answers for the second part of the warm-up

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -14,3 +14,14 @@ numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
+# 1.
+numbers[0] = "ten"
+
+# 2.
+numbers[-1] = 1
+
+# 3.
+print(numbers[2:])
+
+# 4.
+print(9 in numbers)
