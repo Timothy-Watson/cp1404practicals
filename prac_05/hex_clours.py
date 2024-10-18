72a3f3e3ef718685e6ce5b8fa9e7399993c8e83e
Timothy Watson <timothywatson07@gmail.com>
2024-10-18 10:46:02 +1000

    Add colour_to_hex dictionary

---
+++ prac_05/hex_clours.py
@@ -0,0 +1,5 @@
+"""CP1404/CP5632 Practical - Colour names to hex"""
+
+colour_to_hex = {"Absolute Zero": "#0048ba", "Candy Apple Red": "#ff0800", "Green Lizard": "#a7f432",
+                 "Honolulu Blue": "#006db0", "MediumOrchid": "#ba55d3", "School Bus Yellow": "#ffd800",
+                 "Zomp": "#39a78e", "Tropical Rainforest": "#00755e", "Smoky Black": "#100c08", "GhostWhite": "#f8f8ff"}
