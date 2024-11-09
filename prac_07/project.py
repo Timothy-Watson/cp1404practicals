a9845e464c7c0fc70a517cd412dad1506d7e85a7
timmy <timothy.watson@my.jcu.edu.au>
2024-11-09 22:56:54 +1000

    Add a load function to project_management.py and init project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,4 +2,13 @@
 CP1404 practical - project class
 """
 
+
+# Things in project: Name, Start Date,Priority, Cost Estimate, Completion Percentage
 class Project:
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Load values"""
+        self.name = name
+        self.start_date = start_date
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,6 +1,33 @@
 """
 CP1404 practical - Project Management
 estimated time - 60 min
+    start time - 10:30 PM
+      end time -
    actual time -
 """
 
+import datetime
+from project import Project
+
+FILENAME = "projects.txt"
+
+def main():
+    print("Welcome to Pythonic Project Management")
+    projects = load_projects(FILENAME)
+    print(projects)
+
+
+def load_projects(filename):
+    """Load"""
+    projects = []
+    in_file = open(filename, "r")
+    in_file.readline()  # reads the header of the file
+    for line in in_file:
+        parts = line.strip().split("\t")
+        project = Project(parts[0], parts[1], parts[2], parts[3], parts[4])
+        projects.append(project)
+    in_file.close()
+    return projects
+
+
+main()
