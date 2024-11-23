274a54d27c9d8e518b55a70b5204ec75707ec7d8
Timothy Watson <timothywatson07@gmail.com>
2024-11-24 00:15:09 +1000

    Finish taxi_simulator.py

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -13,6 +13,7 @@ def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
     bill_to_date = 0.0
+    total_distance_travelled = 0
     print("Let's drive!")
     print(MENU)
     choice = input(">>> ").lower()
@@ -26,14 +27,40 @@ def main():
         elif choice == "d":
             if current_taxi is None:
                 print("You need to choose a taxi before you can drive")
+            elif current_taxi.fuel == 0:
+                print("Taxi is out of fuel")
             else:
-                pass
+                distance_to_drive = get_valid_distance()
+                current_taxi.start_fare()
+                total_distance_travelled += current_taxi.drive(distance_to_drive)
+                bill_to_date += current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${current_taxi.get_fare():.2f}")
 
         else:
             print("Invalid option")
         print(f"Bill to date: ${bill_to_date:.2f}")
         print(MENU)
         choice = input(">>> ").lower()
+    print(f"Total trip cost: ${bill_to_date}")
+    print(f"Total distance travelled: {total_distance_travelled}km")
+    print("Taxis are now")
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+def get_valid_distance():
+    """Returns a valid distance to drive"""
+    valid_distance = False
+    while not valid_distance:
+        try:
+            distance_to_drive = int(input("Drive how far? "))
+            if distance_to_drive > 0:
+                valid_distance = True
+            else:
+                print("Not a valid distance")
+        except ValueError:
+            print("Not a valid distance")
+    return distance_to_drive  # Will always return a valid distance
 
 
 def get_valid_taxi(taxis):
